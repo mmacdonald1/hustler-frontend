58c77191aeb3b0d73bb18356e9eb53f82c6e90ed
mmacdonald1 <melmacd94@gmail.com>
2019-01-06 13:51:52 -0500

    login thunk

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -1,7 +1,6 @@
 import {setDecks} from './decks'
 
 export const getUser = (token) => {
-
     console.log("in if")
     return (dispatch) => {
       fetch('http://localhost:3000/profile',{
@@ -15,7 +14,32 @@ export const getUser = (token) => {
       dispatch(setDecks(data.user.decks))
       dispatch(setUser(data.user))
     })
+  }
+}
 
+export const loginUser = (username, password) =>{
+  return (dispatch) => {
+    fetch('http://localhost:3000/login', {
+      method:"POST",
+      headers:{
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    }).then(resp => resp.json())
+    .then(data => {
+      console.log('still attempting to login', data, data.user, data.user.decks)
+      if(data.error){
+        alert('incorrect username or password')
+      }else{
+        localStorage.setItem('token', data.token)
+        dispatch(setDecks(data.user.decks))
+        dispatch(setUser(data.user))
+      }
+    })
   }
 }
 
