2beb892eb28be3639e83489f1042bc74d2751cdd
mmacdonald1 <melmacd94@gmail.com>
2019-01-08 16:51:37 -0500

    scooter quiz feature

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,25 +1,34 @@
 import React, {Component} from 'react'
-import {Button} from 'react-bootstrap'
+import {Well,Button} from 'react-bootstrap'
 
 class QuizCard extends Component{
-  state={
-    clicked: false
-  }
 
-  handleCardClick = () =>{
-    this.setState({clicked: !this.state.clicked})
-  }
 
   render(){
-    console.log(this.props,this.props.card)
+    console.log("LOOK AT ME",this.props.cards)
     return(
       <div>
       {this.props.card?(
-        <div onClick={this.handleCardClick}>
-          {this.state.clicked? this.props.card.content: this.props.card.title}
+        <div>
+          {this.props.clicked? (
+            <Well>
+              <h1 className="back-card-title">{this.props.card.title}</h1>
+              <h2>{this.props.card.content}</h2>
+              <Button bsStyle="primary" onClick={this.props.handleAgain}> Again </Button>
+              <Button bsStyle="primary" onClick={this.props.handleGood}> Good </Button>
+              <Button bsStyle="primary" onClick={this.props.handleEasy}> Easy </Button>
+              <Button bsStyle="primary" onClick={this.props.handleNewCard}>Next Card</Button>
+            </Well>
+          )
+            : (
+            <Well>
+              <h1>{this.props.card.title}</h1>
+              <Button bsStyle="primary" onClick={this.props.handleCardClick}> Show Answer </Button>
+              <Button bsStyle="primary" onClick={this.props.handleNewCard}>Next Card</Button>
+            </Well>
+            )}
         </div>
       ): null}
-      <Button bsStyle="primary" onClick={this.props.handleNewCard}>Next Card</Button>
       </div>
     )
   }
--- src/containers/Quiz.js
+++ src/containers/Quiz.js
@@ -6,7 +6,56 @@ class Quiz extends Component{
 
   state = {
     currentIndex: 0,
-    currentCard:this.props.cards[0]
+    currentCard:this.props.cards[0],
+    clicked: false,
+    cards: this.props.cards
+  }
+
+  componentDidMount(){
+    this.setState({
+      currentIndex: 0,
+      currentCard:this.props.cards[0],
+      clicked: false,
+      cards: this.props.cards
+    })
+  }
+
+  handleCardClick = () =>{
+    this.setState({clicked: !this.state.clicked})
+  }
+
+  handleAgain=()=>{
+    let oldCard = this.state.currentCard
+    let newCards = this.state.cards
+    let newIndex = parseInt(this.state.currentIndex)
+    newIndex = newIndex + 2
+    newCards.splice(newIndex,0,oldCard)
+    this.setState({
+      cards: newCards
+    })
+    this.handleNewCard()
+  }
+  handleGood=()=>{
+    let oldCard = this.state.currentCard
+    let newCards = this.state.cards
+    let newIndex = parseInt(this.state.currentIndex)
+    newIndex = newIndex + 6
+    newCards.splice(newIndex,0,oldCard)
+    this.setState({
+      cards: newCards
+    })
+    this.handleNewCard()
+  }
+  handleEasy=()=>{
+    let oldCard = this.state.currentCard
+    let newCards = this.state.cards
+    let newIndex = parseInt(this.state.currentIndex)
+    newIndex = newCards.length
+    newCards.splice(newIndex,0,oldCard)
+    this.setState({
+      cards: newCards
+    })
+    this.handleNewCard()
   }
 
   handleNewCard=()=>{
@@ -16,12 +65,14 @@ class Quiz extends Component{
     if(newCurrentIndex < this.props.cards.length){
       this.setState({
         currentIndex:newCurrentIndex,
-        currentCard: this.props.cards[newCurrentIndex]
+        currentCard: this.props.cards[newCurrentIndex],
+        clicked: false
       })
     }else{
       this.setState({
         currentIndex:0,
-        currentCard: this.props.cards[0]
+        currentCard: this.props.cards[0],
+        clicked: false
       })
     }
   }
@@ -29,7 +80,7 @@ class Quiz extends Component{
   render(){
     return(
       <div>
-        <QuizCard card={this.state.currentCard} handleNewCard={this.handleNewCard}/>
+        <QuizCard card={this.state.currentCard} handleNewCard={this.handleNewCard} handleCardClick={this.handleCardClick} clicked={this.state.clicked} handleEasy={this.handleEasy} handleGood={this.handleGood} handleAgain={this.handleAgain} cards={this.state.cards}/>
       </div>
     )
   }
